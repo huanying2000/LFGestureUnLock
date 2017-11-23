13d27202a4c18ed4a6e2f0c89cb909d6e584514a
ios开发 <[715416878@qq.com]>
2017-11-23 10:58:32 +0800

    创建新工程

---
+++ LFGestureUnLock/LFGestureUnLock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LFGestureUnLock
+//
+//  Created by ios开发 on 2017/11/23.
+//  Copyright © 2017年 ios开发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
